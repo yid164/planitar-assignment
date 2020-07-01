68df70b810bf463ced6bd454658d5264995a8c34
Ken Dong <yid164@mail.usask.ca>
2020-06-30 22:06:56 -0600

    basic functions finished for view component

--- src/components/View.vue
+++ src/components/View.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
-        Hello
+        <header>
+            {{articleName}}
+            <button @click="edit()">Edit</button>
+        </header>
+        <div>
+            <div v-if="articleContent === '' ">
+                <p>No article with this exact name found， Use Edit button in the header to add it</p>
+            </div>
+            <div v-else>
+                <v-html>{{articleContent}}</v-html>
+            </div>
+        </div>
+
+        <button @click="backToAll()">Back To All</button>
+
     </div>
 </template>
 
@@ -9,8 +23,34 @@
         name: "View",
         data(){
             return{
-                articleName: this.$route.params.name
+                articleName: this.$route.params.name,
+                articleContent: ""
             }
+        },
+        methods:{
+            fetchContent:function () {
+                const url = 'articles/'+this.articleName
+                this.axios.get(url, {
+                    headers: {
+                        'Content-Type': '*',
+                    }
+                })
+                    .then((result)=>{
+                        this.articleContent = result.data.content
+                        console.log(result.data)
+                    })
+            },
+
+            backToAll:function () {
+                this.$router.push('/');
+            },
+
+            edit:function () {
+                this.$router.push('/edit/'+this.articleName);
+            }
+        },
+        mounted() {
+            this.fetchContent();
         }
     }
 </script>
