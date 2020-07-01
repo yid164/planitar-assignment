2bdd5a7e9b0b7e80b7198e6c59279bbaf8c09b16
Ken Dong <yid164@mail.usask.ca>
2020-06-30 20:23:11 -0600

    added the edit, home, and view components, and solve the CORS problems

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="home">
+        <h1>Hello</h1>
+        <div>{{all_info}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data(){
+            return {
+                all_info: []
+            }
+        },
+        methods:{
+            fetchInfo:function () {
+                const url = 'articles/'
+                this.axios.get(url, {
+                    headers: {
+                        'Content-Type': '*',
+                    }
+                })
+                    .then((result)=>{
+                        this.all_info = result.data
+                        console.log(result.data)
+                    })
+            }
+        },
+
+        created() {
+            this.fetchInfo()
+        }
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
