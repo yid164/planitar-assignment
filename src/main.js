eac30c0a2ee42e637668010556bc6a14343fa0b0
Ken Dong <yid164@mail.usask.ca>
2020-06-30 20:25:21 -0600

    added vue router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import router from './router'
 
 
 Vue.config.productionTip = false;
@@ -8,5 +9,6 @@ Vue.config.productionTip = false;
 Vue.prototype.axios = axios;
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
